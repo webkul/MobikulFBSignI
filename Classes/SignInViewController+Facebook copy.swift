3ae88bfe32023dba7ef752ded1f66d6d69279eb3
bhavuk.chawla <bhavuk.chawla546@webkul.com>
2018-09-13 18:21:16 +0530

    Code added

---
+++ Classes/SignInViewController+Facebook copy.swift
@@ -0,0 +1,71 @@
+//
+//  SignIn+Fb.swift
+//  Odoo Marketplace
+//
+//  Created by bhavuk.chawla on 24/07/18.
+//  Copyright © 2018 bhavuk.chawla. All rights reserved.
+//
+
+import Foundation
+import FBSDKLoginKit
+//
+
+class MobikulFBSignIn: NSObject{
+    
+    override init() {
+        super.init()
+        let view = UIApplication.shared.delegate?.window
+        if let viewController = view??.rootViewController {
+            self.fbLogin(view: viewController)
+        }
+        
+    }
+    
+    func fbLogin(view: UIViewController) {
+        
+        let fbLoginManager : FBSDKLoginManager = FBSDKLoginManager()
+        fbLoginManager.logIn(withReadPermissions: ["public_profile", "email", "user_friends"], from: view) { (result, error) -> Void in
+            if (error == nil){
+                if let fbloginresult : FBSDKLoginManagerLoginResult = (result)
+                {
+                    if(fbloginresult.isCancelled){
+                        
+                    }
+                    else{
+                        if(fbloginresult.grantedPermissions.contains("email"))
+                        {
+                            self.getFBUserData()
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func getFBUserData(){
+        if((FBSDKAccessToken.current()) != nil){
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
+                if (error == nil){
+                    //everything works print the user data
+                    //                    print("wd",result)
+                    
+                    let Dict  = result as? [String : Any]
+                    var loginDict = [String : Any]()
+                    loginDict["login"] = (Dict?["email"] as? String)
+                    loginDict["name"] = (Dict?["first_name"] as? String)
+                    loginDict["authUserId"] = (Dict?["id"] as? String)
+                    loginDict["authProvider"] = "FACEBOOK"
+                    loginDict["isSocialLogin"] = true
+                    loginDict["password"] = ""
+                    NotificationCenter.default.post(name:  NSNotification.Name(rawValue: "MobikulFBSignIn"), object: nil, userInfo: loginDict)
+                }
+                
+            })
+        }
+    }
+    
+}
+
+
+
+
--- MobikulFBSignI.podspec
+++ MobikulFBSignI.podspec
@@ -30,7 +30,7 @@ TODO: Add long description of the pod here.
 
   s.ios.deployment_target = '8.0'
 
-  s.source_files = 'MobikulFBSignI/Classes/**/*'
+  s.source_files = "MobikulFBSignI/Classes/*"
   
   # s.resource_bundles = {
   #   'MobikulFBSignI' => ['MobikulFBSignI/Assets/*.png']
@@ -38,5 +38,7 @@ TODO: Add long description of the pod here.
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
-  # s.dependency 'AFNetworking', '~> 2.3'
+   s.dependency 'FBSDKCoreKit'
+   s.dependency 'FBSDKShareKit'
+   s.dependency 'FBSDKLoginKit'
 end
